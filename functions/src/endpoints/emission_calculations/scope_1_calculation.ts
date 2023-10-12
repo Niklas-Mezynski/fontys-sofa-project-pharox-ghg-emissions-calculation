4f4766ebe0e9cafa2b84572ef181b4b47d819e73
Zali-Masodi <60880359+Zali-Masodi@users.noreply.github.com>
2023-10-12 20:36:08 +0200

    finished scope 1 calculations

---
+++ functions/src/endpoints/emission_calculations/scope_1_calculation.ts
@@ -0,0 +1,66 @@
+import { HttpStatusCode } from "axios";
+import { onRequest } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import { z } from "zod";
+import { EmissionFactorService } from "../../logic/emission_factors/emission_factor_service";
+import { parseZodError } from "../../utils/functions";
+import fetch from 'node-fetch';
+
+
+const queryInputSchema = z.object({
+  usedFuel: z.coerce.number().min(1),
+  activityId: z.string().min(1)
+});
+
+export const Scope1Calculation = onRequest(async (request, response) => {
+  const parseResult = queryInputSchema.safeParse(request.query);
+
+  if (!parseResult.success) {
+    response.status(HttpStatusCode.BadRequest).json({
+      status: HttpStatusCode.BadRequest,
+      message: parseZodError(parseResult.error.issues),
+    });
+    return;
+  }
+
+  const { activityId } = parseResult.data 
+  const { usedFuel } = parseResult.data
+  
+  const activity = await EmissionFactorService.getByActivityId(activityId)  
+
+  const factor = activity?.factor
+
+  const apiUrl = 'https://emissioncalculationsimple-u2tzkd7k6q-uc.a.run.app';
+
+  const postData = {
+    usedFuel: usedFuel,
+    emissionFactor: factor
+  };
+
+
+  logger.info(
+    "Provided Fuel: " +
+      usedFuel +
+      "\n Provided Activity ID: " +
+      activityId
+  );
+
+
+
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(postData),
+  })
+    .then((APIresponse) => APIresponse.json())
+    .then((data) => {
+      response.send(data)
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+
+});
