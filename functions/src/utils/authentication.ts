3fc1214b2126bf11c2bf7d561fe512f31fd29167
Joel Sebastian <70883097+joelSebastianDelgado@users.noreply.github.com>
2023-10-26 14:37:53 +0200

    Use Bearer token and add example

--- functions/src/utils/authentication.ts
+++ functions/src/utils/authentication.ts
@@ -5,15 +5,17 @@ import { HttpStatusCode } from "axios";
    * Function to determinate whether the provided token is valid
    * @param {string} token - the provided authentication token
    */
-export function authenticate(token: string): void {
-  if (!process.env.AUTH_TOKEN) {
+export function authenticate(token: string | undefined): void {
+  if (!process.env.AUTH_TOKEN || !token) {
     throw new CustomError({
       status: HttpStatusCode.NotFound,
       message: "No authentication token found",
     });
   }
 
-  if (process.env.AUTH_TOKEN !== token) {
+  const tokenValue = token.split("Bearer ")[1];
+
+  if (process.env.AUTH_TOKEN !== tokenValue) {
     throw new CustomError({
       status: HttpStatusCode.Forbidden,
       message: "Wrong authentication token",
--- functions/tests/utils/authentication.test.ts
+++ functions/tests/utils/authentication.test.ts
@@ -3,19 +3,19 @@ import { authenticate } from "../../src/utils/authentication";
 import { CustomError } from "../../src/utils/errors";
 
 describe("Utils - Authentication", () => {
-  beforeAll( () => {
+  beforeEach( () => {
     process.env.AUTH_TOKEN = "P95SfxvCPO8T2jC2tLCGK8MepjpiOVE00qI5k4MJIwYfKrray7J4zEq0fPLa7Z2k";
   });
 
   test("User is authenticated", async () => {
-    const userToken = "P95SfxvCPO8T2jC2tLCGK8MepjpiOVE00qI5k4MJIwYfKrray7J4zEq0fPLa7Z2k";
+    const userToken = "Bearer P95SfxvCPO8T2jC2tLCGK8MepjpiOVE00qI5k4MJIwYfKrray7J4zEq0fPLa7Z2k";
 
     expect((() => {
       authenticate(userToken);
     })).not.toThrowError();
   });
 
-  test("User is not authenticated throws error'", async () => {
+  test("Not valid authentication token throws error'", async () => {
     const userToken = "5GJqmDbyYb9VCIP1JRlbGRBYJcUOjBUdwzKO7RzxKdE0Co3aaAPJpa10lq7IxCPe";
     const error = new CustomError({
       status: HttpStatusCode.Forbidden,
@@ -31,8 +31,23 @@ describe("Utils - Authentication", () => {
     }
   });
 
-  test("No availble authentication token throws error'", async () => {
-    const userToken = "5GJqmDbyYb9VCIP1JRlbGRBYJcUOjBUdwzKO7RzxKdE0Co3aaAPJpa10lq7IxCPe";
+  test("No availble authentication token throws error - A", async () => {
+    const error = new CustomError({
+      status: HttpStatusCode.NotFound,
+      message: "No authentication token found",
+    });
+
+    try {
+      authenticate(undefined);
+    } catch (err) {
+      expect(err).toBeInstanceOf(CustomError);
+      expect((<CustomError>err).message).toBe(error.message);
+      expect((<CustomError>err).status).toBe(error.status);
+    }
+  });
+
+  test("No availble authentication token throws error - B", async () => {
+    const userToken = "Bearer 5GJqmDbyYb9VCIP1JRlbGRBYJcUOjBUdwzKO7RzxKdE0Co3aaAPJpa10lq7IxCPe";
     delete process.env.AUTH_TOKEN;
 
     const error = new CustomError({
