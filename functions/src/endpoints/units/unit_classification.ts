e376022663d5f0ae128264d192319499b083705f
Patrick <vanleipsigpatrick@gmail.com>
2023-10-27 10:12:18 +0200

    Cleaned up imports and small bugfix

--- functions/src/endpoints/units/unit_classification.ts
+++ functions/src/endpoints/units/unit_classification.ts
@@ -1,9 +1,6 @@
-import { HttpStatusCode } from "axios";
 import { onRequest } from "firebase-functions/v2/https";
 import { z } from "zod";
-import { EmissionFactorService } from "../../logic/emission_factors/emission_factor_service";
-import { SimpleCalculationService } from "../../logic/emission_calculations/emission_calculator_service";
-import { parseZodError, validateInput } from "../../utils/functions";
+import { validateInput } from "../../utils/functions";
 import { classifyUnitType } from "../../logic/units/unit_classification_service";
 
 const queryInputSchema = z.object({
@@ -22,5 +19,5 @@ export const UnitClassification = onRequest(async (request, response) => {
 
   const unitType = classifyUnitType(requestBody.unit);
 
-  response.json({ unit: unitType }).sendStatus(200);
+  response.json({ unit: unitType }).status(200).send();
 });
