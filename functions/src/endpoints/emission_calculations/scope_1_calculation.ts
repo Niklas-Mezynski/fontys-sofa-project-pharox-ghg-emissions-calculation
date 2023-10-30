a505d62093c1094b5c8766fc0acd55e1ca65e591
Joel Sebastian <70883097+joelSebastianDelgado@users.noreply.github.com>
2023-10-30 08:49:27 +0100

    Fix build error

--- functions/src/endpoints/emission_calculations/scope_1_calculation.ts
+++ functions/src/endpoints/emission_calculations/scope_1_calculation.ts
@@ -1,70 +1,71 @@
-// import { HttpStatusCode } from "axios";
-// import { onRequest } from "firebase-functions/v2/https";
-// import { z } from "zod";
-// import { EmissionFactorService } from "../../logic/emission_factors/emission_factor_service";
-// import { EmissionCalculatorService } from "../../logic/emission_calculations/emission_calculator_service";
-// import { parseZodError } from "../../utils/functions";
+import { HttpStatusCode } from "axios";
+import { onRequest } from "firebase-functions/v2/https";
+import { z } from "zod";
+import { EmissionFactorService } from "../../logic/emission_factors/emission_factor_service";
+import { parseZodError } from "../../utils/functions";
 
-// const queryInputSchema = z.object({
-//   usedFuel: z.coerce.number().min(1),
-//   activityId: z.coerce.string().min(1),
-// });
+const queryInputSchema = z.object({
+  usedFuel: z.coerce.number().min(1),
+  activityId: z.coerce.string().min(1),
+});
 
-// export const Scope1Calculation = onRequest(async (request, response) => {
-//   const parseResult = queryInputSchema.safeParse(request.query);
+export const Scope1Calculation = onRequest(async (request, response) => {
+  const parseResult = queryInputSchema.safeParse(request.query);
 
-//   if (!parseResult.success) {
-//     response.status(HttpStatusCode.BadRequest).json({
-//       status: HttpStatusCode.BadRequest,
-//       message: parseZodError(parseResult.error.issues),
-//     });
-//     return;
-//   }
+  if (!parseResult.success) {
+    response.status(HttpStatusCode.BadRequest).json({
+      status: HttpStatusCode.BadRequest,
+      message: parseZodError(parseResult.error.issues),
+    });
+    return;
+  }
 
-//   const { activityId } = parseResult.data;
-//   const { usedFuel } = parseResult.data;
+  const { activityId } = parseResult.data;
+  const { usedFuel } = parseResult.data;
 
-//   const activity = await EmissionFactorService.getByActivityId(activityId);
+  const activity = await EmissionFactorService.getByActivityId(activityId);
 
-//   const factor = activity?.factor;
+  const factor = activity?.factor;
 
-//   const postData = {
-//     usedFuel: usedFuel,
-//     emissionFactor: factor,
-//   };
+  const postData = {
+    usedFuel: usedFuel,
+    emissionFactor: factor,
+  };
+
+  response.status(200);
 //   const result = SimpleCalculationService.simpleEmissionCalculation(postData);
 
 //   response.status(200).send(result);
-//   /*
-//   const apiUrl = 'https://emissioncalculationsimple-u2tzkd7k6q-uc.a.run.app';
+  /*
+  const apiUrl = 'https://emissioncalculationsimple-u2tzkd7k6q-uc.a.run.app';
 
-//   const postData = {
-//     usedFuel: usedFuel,
-//     emissionFactor: factor
-//   };
-
-
-//   logger.info(
-//     "Provided Fuel: " +
-//       usedFuel +
-//       "\n Provided Activity ID: " +
-//       activityId
-//   );
+  const postData = {
+    usedFuel: usedFuel,
+    emissionFactor: factor
+  };
 
 
-//   fetch(apiUrl, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(postData),
-//   })
-//     .then((APIresponse) => APIresponse.json())
-//     .then((data) => {
-//       response.send(data)
-//     })
-//     .catch((error) => {
-//       console.error('Error:', error);
-//     });
-// */
-// });
+  logger.info(
+    "Provided Fuel: " +
+      usedFuel +
+      "\n Provided Activity ID: " +
+      activityId
+  );
+
+
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(postData),
+  })
+    .then((APIresponse) => APIresponse.json())
+    .then((data) => {
+      response.send(data)
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+*/
+});
