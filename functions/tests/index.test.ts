64146555011df8a9447d36a9ebbfdd7127f91d5e
Zali-Masodi <60880359+Zali-Masodi@users.noreply.github.com>
2023-10-31 13:24:49 +0100

    unit_classification_service_tests

--- functions/tests/index.test.ts
+++ functions/tests/index.test.ts
@@ -1,5 +1,7 @@
 // import * as indexFunctions from "../src/index";
 
+import { classifyUnitType } from "../src/logic/units/unit_classification_service";
+
 describe("Index - Hello World", () => {
   test("Hello World function returns 'Hello world!'", async () => {
     // A fake request object, with req.query.text set to 'input'
@@ -18,5 +20,52 @@ describe("Index - Hello World", () => {
     // await indexFunctions.helloWorld(req as any, res as any);
 
     expect(1).toBe(1);
+  });
+});
+
+
+describe('testing_volume_unit_classification_service', () => {
+  test('The unit provided should return the correct unit type', () => {
+    
+    expect(classifyUnitType("ml")).toBe("VOLUME");
+    expect(classifyUnitType("l")).toBe("VOLUME");
+    expect(classifyUnitType("tsp")).toBe("VOLUME");
+    expect(classifyUnitType("tbsp")).toBe("VOLUME");
+
+  });
+});
+
+describe('testing_weight_unit_classification_service', () => {
+  test('The unit provided should return the correct unit type', () => {
+    
+    expect(classifyUnitType("g")).toBe("WEIGHT");
+    expect(classifyUnitType("kg")).toBe("WEIGHT");
+    expect(classifyUnitType("oz")).toBe("WEIGHT");
+    expect(classifyUnitType("lb")).toBe("WEIGHT");
+
+  });
+});
+
+describe('testing_distance_unit_classification_service', () => {
+  test('The unit provided should return the correct unit type', () => {
+    
+    expect(classifyUnitType("km")).toBe("DISTANCE");
+    expect(classifyUnitType("cm")).toBe("DISTANCE");
+    expect(classifyUnitType("m")).toBe("DISTANCE");
+    expect(classifyUnitType("in")).toBe("DISTANCE");
+    expect(classifyUnitType("ft")).toBe("DISTANCE");
+    expect(classifyUnitType("mi")).toBe("DISTANCE");
+
+  });
+});
+
+describe('testing_unit_classification_service_Wrong_input', () => {
+  test('The unit provided should return "UNKNOWN"', () => {
+    
+    expect(classifyUnitType("a")).toBe("UNKNOWN");
+    expect(classifyUnitType("trtrt")).toBe("UNKNOWN");
+    expect(classifyUnitType("1111")).toBe("UNKNOWN");
+    expect(classifyUnitType("")).toBe("UNKNOWN");
+
   });
 });
