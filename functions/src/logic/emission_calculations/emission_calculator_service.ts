53dabda7719ea0a256170d84ee4e819d55bf1e56
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2023-10-31 14:10:08 +0100

    Add better error handling

--- functions/src/logic/emission_calculations/emission_calculator_service.ts
+++ functions/src/logic/emission_calculations/emission_calculator_service.ts
@@ -31,73 +31,87 @@ async function performEmissionCalculation(
 
   await Promise.all(
     calculationInput.transportParts.map(async (transportPart, index) => {
-      // Get the emission factor
-      const emissionFactor = await getEmissionFactor(
-        transportPart.transportDetails
-      );
+      try {
+        // Get the emission factor
+        const { producedEmissions, factorToUse, mappedEmissionFactor } =
+          await calculateTransportActivity(transportPart);
 
-      // --- Unit conversion ---
-
-      const providedUnitType = classifyUnitType(
-        transportPart.transportDetails.consumedFuel.unit
-      );
-
-      const mappedEmissionFactor = {
-        ...emissionFactor,
-        factors: emissionFactor.factors.map((factor) => ({
-          ...factor,
-          producedUnit: factor.unit.split("_PER_")[0].toLowerCase(),
-          perUnit: factor.unit.split("_PER_")[1].toLowerCase(), // TODO: Check that this toLowerCase() does not produce any errors
-        })),
-      };
-
-      // Get the factor with the same unit type as the provided unit type
-      const factorToUse = mappedEmissionFactor.factors.find(
-        (factor) => classifyUnitType(factor.perUnit) === providedUnitType
-      );
-
-      if (!factorToUse) {
+        // Add the emission to the report
+        calculationReport.transportActivities[index] = {
+          producedEmissions,
+          unit: `${factorToUse.producedUnit} / ${factorToUse.perUnit}`,
+          usedEmissionFactor: {
+            ...mappedEmissionFactor,
+            factors: factorToUse,
+          },
+        };
+      } catch (error) {
         throw new CustomError({
           status: HttpStatusCode.BadRequest,
-          message: `No emission factor found for unit type ${providedUnitType}`,
+          message: `Error while calculating emission for transport part ${index}`,
+          details: error,
         });
       }
-
-      // Convert the consumed fuel to the unit type of the emission factor
-      const convertedConsumedFuel = UnitConversionService.convertUnits(
-        transportPart.transportDetails.consumedFuel.unit,
-        factorToUse.perUnit,
-        transportPart.transportDetails.consumedFuel.value
-      );
-
-      // --- Emission calculation ---
-
-      // Calculate the emission
-      const producedEmissions = {
-        tankToWheel: factorToUse.factor.TTW
-          ? convertedConsumedFuel.value * factorToUse.factor.TTW
-          : null,
-        wellToTank: factorToUse.factor.WTT
-          ? convertedConsumedFuel.value * factorToUse.factor.WTT
-          : null,
-        wellToWheel: factorToUse.factor.WTW
-          ? convertedConsumedFuel.value * factorToUse.factor.WTW
-          : null,
-      };
-
-      // Add the emission to the report
-      calculationReport.transportActivities[index] = {
-        producedEmissions,
-        unit: `${factorToUse.producedUnit} / ${factorToUse.perUnit}`,
-        usedEmissionFactor: {
-          ...mappedEmissionFactor,
-          factors: factorToUse,
-        },
-      };
     })
   );
   calculationReport.totalEmissions = -1; // TODO: Calculate total emissions
   return calculationReport;
+}
+
+async function calculateTransportActivity(
+  transportPart: FreightEmissionCalculationInput["transportParts"][number]
+) {
+  const emissionFactor = await getEmissionFactor(
+    transportPart.transportDetails
+  );
+
+  // --- Unit conversion ---
+  const providedUnitType = classifyUnitType(
+    transportPart.transportDetails.consumedFuel.unit
+  );
+
+  const mappedEmissionFactor = {
+    ...emissionFactor,
+    factors: emissionFactor.factors.map((factor) => ({
+      ...factor,
+      producedUnit: factor.unit.split("_PER_")[0].toLowerCase(),
+      perUnit: factor.unit.split("_PER_")[1].toLowerCase(), // TODO: Check that this toLowerCase() does not produce any errors
+    })),
+  };
+
+  // Get the factor with the same unit type as the provided unit type
+  const factorToUse = mappedEmissionFactor.factors.find(
+    (factor) => classifyUnitType(factor.perUnit) === providedUnitType
+  );
+
+  if (!factorToUse) {
+    throw new CustomError({
+      status: HttpStatusCode.BadRequest,
+      message: `No emission factor found for unit type ${providedUnitType}`,
+    });
+  }
+
+  // Convert the consumed fuel to the unit type of the emission factor
+  const convertedConsumedFuel = UnitConversionService.convertUnits(
+    transportPart.transportDetails.consumedFuel.unit,
+    factorToUse.perUnit,
+    transportPart.transportDetails.consumedFuel.value
+  );
+
+  // --- Emission calculation ---
+  // Calculate the emission
+  const producedEmissions = {
+    tankToWheel: factorToUse.factor.TTW
+      ? convertedConsumedFuel.value * factorToUse.factor.TTW
+      : null,
+    wellToTank: factorToUse.factor.WTT
+      ? convertedConsumedFuel.value * factorToUse.factor.WTT
+      : null,
+    wellToWheel: factorToUse.factor.WTW
+      ? convertedConsumedFuel.value * factorToUse.factor.WTW
+      : null,
+  };
+  return { producedEmissions, factorToUse, mappedEmissionFactor };
 }
 
 /**
