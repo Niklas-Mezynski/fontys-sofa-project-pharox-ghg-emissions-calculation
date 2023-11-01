fceb0df081772fbd81720617bba77dc4ecff7c83
Zali-Masodi <60880359+Zali-Masodi@users.noreply.github.com>
2023-11-01 18:06:47 +0100

    restructured test folder

--- functions/tests/index.test.ts
+++ functions/tests/index.test.ts
@@ -71,47 +71,13 @@ describe('testing_unit_classification_service_Wrong_input', () => {
   });
 });
 
-describe('testing_unit_supported', () => {
-  test('The unit provided should return "true"', () => {
-    
-    expect(UnitConversionService.verifyIfUnitIsSupported("ml")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("l")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("tsp")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("tbsp")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("g")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("kg")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("oz")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("lb")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("km")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("cm")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("m")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("in")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("ft")).toBe(true);
-    expect(UnitConversionService.verifyIfUnitIsSupported("mi")).toBe(true);
-
-  });
-});
-
-describe('testing_unit_unsupported', () => {
-  test('The unit provided should return "false"', () => {
-
-    expect(UnitConversionService.verifyIfUnitIsSupported("a")).toBe(false);
-    expect(UnitConversionService.verifyIfUnitIsSupported("h")).toBe(false);
-    expect(UnitConversionService.verifyIfUnitIsSupported("km2")).toBe(false);
-    expect(UnitConversionService.verifyIfUnitIsSupported("")).toBe(false);
-    expect(UnitConversionService.verifyIfUnitIsSupported("111")).toBe(false);
-    expect(UnitConversionService.verifyIfUnitIsSupported("st")).toBe(false);
-    
-  });
-});
-
 describe('testing_unit_conversion', () => {
   test('The unit and value provided should return the coresponding value in desired units', () => {
 
-    expect(UnitConversionService.convertUnits("km", "m", 1)).toBe(1000);
-    expect(UnitConversionService.convertUnits("kg", "g", 1)).toBe(1000);
+    expect(UnitConversionService.convertUnits("km", "m", 1).value).toBe(1000);
+    expect(UnitConversionService.convertUnits("kg", "g", 1).value).toBe(1000);
     //expect(UnitConversionService.convertUnits("m", "ft", 1)).toBe(3.2);
-    expect(UnitConversionService.convertUnits("ml", "l", 1000)).toBe(1);
+    expect(UnitConversionService.convertUnits("ml", "l", 1000).value).toBe(1);
     //expect(UnitConversionService.convertUnits("oz", "g", 1)).toBe(28.3495);
     //expect(UnitConversionService.convertUnits("mi", "km", 1)).toBe(1.60934);
     //expect(UnitConversionService.convertUnits("km", "l", 1)).toBe(1000);
---
+++ functions/tests/logic/units.test.ts
@@ -0,0 +1,69 @@
+import { classifyUnitType } from "../../src/logic/units/unit_classification_service";
+import { UnitConversionService } from "../../src/logic/units/unit_conversion_service";
+//import { CustomError } from "../../src/utils/errors";
+
+describe('testing_volume_unit_classification_service', () => {
+    test('The unit provided should return the correct unit type', () => {
+      
+      expect(classifyUnitType("ml")).toBe("VOLUME");
+      expect(classifyUnitType("l")).toBe("VOLUME");
+      expect(classifyUnitType("tsp")).toBe("VOLUME");
+      expect(classifyUnitType("tbsp")).toBe("VOLUME");
+  
+    });
+  });
+  
+  describe('testing_weight_unit_classification_service', () => {
+    test('The unit provided should return the correct unit type', () => {
+      
+      expect(classifyUnitType("g")).toBe("WEIGHT");
+      expect(classifyUnitType("kg")).toBe("WEIGHT");
+      expect(classifyUnitType("oz")).toBe("WEIGHT");
+      expect(classifyUnitType("lb")).toBe("WEIGHT");
+  
+    });
+  });
+  
+  describe('testing_distance_unit_classification_service', () => {
+    test('The unit provided should return the correct unit type', () => {
+      
+      expect(classifyUnitType("km")).toBe("DISTANCE");
+      expect(classifyUnitType("cm")).toBe("DISTANCE");
+      expect(classifyUnitType("m")).toBe("DISTANCE");
+      expect(classifyUnitType("in")).toBe("DISTANCE");
+      expect(classifyUnitType("ft")).toBe("DISTANCE");
+      expect(classifyUnitType("mi")).toBe("DISTANCE");
+  
+    });
+  });
+  
+  describe('testing_unit_classification_service_Wrong_input', () => {
+    test('The unit provided should return "UNKNOWN"', () => {
+      
+      expect(classifyUnitType("a")).toBe("UNKNOWN");
+      expect(classifyUnitType("trtrt")).toBe("UNKNOWN");
+      expect(classifyUnitType("1111")).toBe("UNKNOWN");
+      expect(classifyUnitType("")).toBe("UNKNOWN");
+  
+    });
+  });
+  
+  describe('testing_unit_conversion', () => {
+    test('The unit and value provided should return the coresponding value in desired units', () => {
+  
+      expect(UnitConversionService.convertUnits("km", "m", 1).value).toBe(1000);
+      expect(UnitConversionService.convertUnits("kg", "g", 1).value).toBe(1000);
+      expect(UnitConversionService.convertUnits("m", "ft", 1).value).toBeCloseTo(3.28);
+      expect(UnitConversionService.convertUnits("ml", "l", 1000).value).toBe(1);
+      expect(UnitConversionService.convertUnits("oz", "g", 1).value).toBeCloseTo(28.349);
+      expect(UnitConversionService.convertUnits("mi", "km", 1).value).toBeCloseTo(1.609);      
+    });
+  });
+
+  describe('testing_unit_conversion_throws_an_error', () => {
+    test('The unit conversion should throw an error due to units being of a different unit type', () => {
+
+      expect(UnitConversionService.convertUnits("mi", "l", 1)).toThrowError();
+    
+    });
+  });
