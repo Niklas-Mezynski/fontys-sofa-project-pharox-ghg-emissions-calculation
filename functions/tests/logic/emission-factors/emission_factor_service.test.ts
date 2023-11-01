9ab0011e20953890b25e531c32fbf5ef49742cf1
Joel Sebastian <70883097+joelSebastianDelgado@users.noreply.github.com>
2023-11-01 13:56:56 +0100

    Trying to mock firebase for testing

--- functions/tests/logic/emission-factors/emission_factor_service.test.ts
+++ functions/tests/logic/emission-factors/emission_factor_service.test.ts
@@ -1,27 +1,27 @@
-import FirestoreMock from "../../helpers/firestore-mock";
-import firebase from "firebase/app";
+import FirestoreMock from "../../helpers/firestore_mock";
+// import firebase from "firebase/app";
 import "firebase/firestore";
 
 describe("Emission factors - Fuel emission factors", () => {
-  const firestoreMock = new FirestoreMock;
+  const firestoreMock = new FirestoreMock();
 
   beforeEach(() => {
-    firebase.firestore = firestoreMock;
+    // firebase.firestore = firestoreMock;
     firestoreMock.reset();
   });
 
   test("Create fuel emission factor returns created factor", async () => {
     it("does something", (done) => {
       firestoreMock.mockAddReturn = { id: "test-id" };
-      firebase.firestore.collection("foobar")
-        .add({foo: "bar"})
-        .then((res: { id: any; }) => {
-          expect(firestoreMock.mockCollection).toBeCalledWith("foobar");
-          expect(firestoreMock.mockAdd).toBeCalledWith({foo: "bar"});
-          expect(res.id).toEqual("test-id");
-          done();
-        })
-        .catch(done);
+      // firebase.firestore.collection("foobar")
+      //   .add({foo: "bar"})
+      //   .then((res: { id: any; }) => {
+      //     expect(firestoreMock.mockCollection).toBeCalledWith("foobar");
+      //     expect(firestoreMock.mockAdd).toBeCalledWith({foo: "bar"});
+      //     expect(res.id).toEqual("test-id");
+      //     done();
+      //   })
+      //   .catch(done);
     });
   });
 });
