d9ab6e8fe11a719284193d23d3d395dae49d341b
Joel Sebastian <j.sebastian@student.fontys.nl>
2023-11-02 10:32:11 +0100

    Fix Error with git

--- functions/tests/logic/emission-factors/emission_factor_service.test.ts
+++ functions/tests/logic/emission-factors/emission_factor_service.test.ts
@@ -1,29 +1,57 @@
-import FirestoreMock from "../../helpers/firestore_mock";
-// import firebase from "firebase/app";
-import "firebase/firestore";
+import { v4 as uuid } from "uuid";
+import { EmissionFactorService } from "../../../src/logic/emission_factors/emission_factor_service";
 
 describe("Emission factors - Fuel emission factors", () => {
-  const firestoreMock = new FirestoreMock();
+  // const firestoreMock = new FirestoreMock();
+
+  // const fuelEmissionFactorsCollection = "fuel_emission_factors";
+  // const fuelEmissionFactor = {
+  //   "source": "GLEC",
+  //   "fuel": {
+  //     "code": "MARINE_GAS_OIL",
+  //     "description": "Marine gas oil",
+  //   },
+  //   "factors": [{
+  //     "unit": "KG_CO2E_PER_KG",
+  //     "factor": {
+  //       "WTT": 0.68,
+  //       "TTW": 3.24,
+  //       "WTW": 3.92,
+  //     },
+  //   	}, {
+  //     "unit": "KG_CO2E_PER_L",
+  //     "factor": {
+  //       "WTT": 0.61,
+  //       "TTW": 2.88,
+  //       "WTW": 3.49,
+  //     },
+  //   },
+  //   ],
+  //   "region": "EU",
+  // }
+
+  // const mockQueryResponse = jest.fn()
+  // mockQueryResponse.mockResolvedValue(fuelEmissionFactor)
+
+  // jest.mock("firebase-admin", () => ({
+  //   initializeApp: jest.fn(),
+  //   firestore: () => ({
+  //     collection: jest.fn(path => ({
+  //       doc: jest.fn(id => ({
+  //         get: mockQueryResponse,
+  //       })),
+  //     })), 
+  //   }),
+  // }))
 
   beforeEach(() => {
     // firebase.firestore = firestoreMock;
-    firestoreMock.reset();
+    // firestoreMock.reset();
   });
 
-  test("Create fuel emission factor returns created factor", async () => {
-    it("does something", (done) => {
-      firestoreMock.mockAddReturn = { id: "test-id" };
-      // firebase.firestore.collection("foobar")
-      //   .add({foo: "bar"})
-      //   .then((res: { id: any; }) => {
-      //     expect(firestoreMock.mockCollection).toBeCalledWith("foobar");
-      //     expect(firestoreMock.mockAdd).toBeCalledWith({foo: "bar"});
-      //     expect(res.id).toEqual("test-id");
-      //     done();
-      //   })
-      //   .catch(done);
-    });
-  });
+  test("Get Fuel Emission Factor by ID returns desired fuel emission factor", async () => {
+    console.log(await EmissionFactorService.getFuelEmissionFactorById(uuid()));
+  }, );
 });
 
 describe("Emission factors - Intensity emission factors", () => {
