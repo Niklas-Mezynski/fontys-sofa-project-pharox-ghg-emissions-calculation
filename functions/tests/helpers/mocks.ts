c6cfee533a1b11c9b420d632c8be51567bbfb2dc
Joel Sebastian <j.sebastian@student.fontys.nl>
2023-11-15 16:38:08 +0100

    Add missing documentation to the Mock class

--- functions/tests/helpers/mocks.ts
+++ functions/tests/helpers/mocks.ts
@@ -1,19 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+/**
+ * Interface to simulate Firestore QuerySnapshot type/object
+ */
 export interface QuerySnapshot<T> {
 	docs: DocumentSnapshot<T>[];
 }
 
+/**
+ * Interface to simulate Firestore DocumentSnapshot type/object
+ */
 export interface DocumentSnapshot<T> {
 	data(): T;
 }
 
+/**
+ * Class to mock the Firebase and Firestore methods
+ */
 export class Mock<T> {
   private _documentSnapshot!: DocumentSnapshot<T>;
   private _querySnapshot!: QuerySnapshot<T>;
   private _data: T[];
-  private _appMock: any;
-  private _firestoreMock: any;
 
   constructor(data: T[]) {
     this._data = data;
@@ -21,25 +28,42 @@ export class Mock<T> {
     this.setDocumentSnapshot(data[0]);
   }
 
+  /**
+   * Getter for the DocumentSnapshot
+   * @returns {DocumentSnapshot<T>} The document snapshot
+   */
   public getDocumentSnapshot(): DocumentSnapshot<T> {
     return this._documentSnapshot;
   }
 
+  /**
+   * Setter for the DocumentSnapshot
+   * @param {T} item - The item to be set
+   * @returns {void}
+   */
   public setDocumentSnapshot(item: T): void {
     this._documentSnapshot = {
       data: () => item,
     };
   }
 
+  /**
+   * Getter for the QuerySnapshot
+   * @returns {QuerySnapshot<T>} The query snapshot
+   */
   public getQuerySnapshot(): QuerySnapshot<T> {
     return this._querySnapshot;
   }
 
+  /**
+   * Setter for the QuerySnapshot
+   * @param {T[]} data - The data to be set
+   */
   public setQuerySnapshot(data: T[]) {
     const querySnapshot: QuerySnapshot<T> = {
       docs: [],
     }
-		
+
     for (const item of data) {
       querySnapshot.docs.push({
         data: () => item,
@@ -49,6 +73,10 @@ export class Mock<T> {
     this._querySnapshot = querySnapshot;
   }
 
+  /**
+   * Reset the mocked values to the default data
+   * @returns {void}
+   */
   public resetMockToDefaultData(): void {
     this.setQuerySnapshot(this._data);
     this.setDocumentSnapshot(this._data[0]);
@@ -59,7 +87,7 @@ export class Mock<T> {
 	 * @returns {void}
 	 */
   public mockFirebaseApplication(): void {
-    this._appMock = jest.mock("firebase-admin/app", () => ({
+    jest.mock("firebase-admin/app", () => ({
       initializeApp: jest.fn(),
     }));
   }
@@ -69,7 +97,7 @@ export class Mock<T> {
 	 * @returns {void}
 	 */
   public mockFirestore(): void {
-    this._firestoreMock = jest.mock("firebase-admin/firestore", () => ({
+    jest.mock("firebase-admin/firestore", () => ({
       getFirestore: jest.fn(() => ({
         collection: jest.fn((path) => ({
           get: jest.fn().mockResolvedValue(this.getQuerySnapshot()),
@@ -79,14 +107,8 @@ export class Mock<T> {
           doc: jest.fn((ref) => ({
             get: jest.fn().mockResolvedValue(this.getDocumentSnapshot()),
           })),
-        })), 
+        })),
       })),
     }));
   }
-
-  public clearMocks(): void {
-    if(this._appMock) this._appMock.mockClear();
-    if(this._firestoreMock) this._firestoreMock.mockClear();
-  }
-
 }
