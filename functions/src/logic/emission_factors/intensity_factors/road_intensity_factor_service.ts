9cae437225c5c9bbe3849560538aee4dca0b7940
Joel Sebastian <j.sebastian@student.fontys.nl>
2023-11-17 14:24:50 +0100

    Fix compile errors

--- functions/src/logic/emission_factors/intensity_factors/road_intensity_factor_service.ts
+++ functions/src/logic/emission_factors/intensity_factors/road_intensity_factor_service.ts
@@ -1,4 +1,4 @@
-import { FreightEmissionCalculationInput, RoadTransportDetails } from "../../../models/emission_calculations/emission_calculation_model";
+import { RoadTransportDetails } from "../../../models/emission_calculations/emission_calculation_model";
 import {
   RoadIntensityFactor,
   roadIntensityFactorSchema,
@@ -61,36 +61,41 @@ async function createRoadIntensityFactors(
 async function getSpecificIntensityFactor(
   data: RoadTransportDetails,
   region: string ): Promise<RoadIntensityFactor>{
-    
-    const filter = Filter.and(
-      Filter.where("fuel.code", "==", data.fuelCode ?? null),
-      Filter.or(
-        Filter.where("region", "==", region),
-        Filter.where("region", "==", "INTERNATIONAL")
-      ),
-      Filter.or(
-        Filter.where("vehicle.weight.lower", ">=", data.vehicle.weight.value),
-        Filter.where("vehicle.weight.upper", "<=", data.vehicle.weight.value),
-      ),
-      Filter.where("vehicle.weight.unit", "==", data.vehicle.weight.unit),
-      Filter.where("vehicle.code", "==", data.vehicle.code ?? null),
-      Filter.where("vehicle.engineType", "==", data.vehicle.engineType ?? null),
-      Filter.where("characteristics.loadCharacteristic", "==", data.characteristics.loadCharacteristic ?? null),
-      Filter.where("characteristics.loadFactor", "==", data.characteristics.loadFactor ?? null),
-      Filter.where("characteristics.combinedLoadFactorEmptyRunning", "==", data.characteristics.combinedLoadFactorEmptyRunning ?? null),
-      Filter.where("characteristics.emptyRunning", "==", data.characteristics.emptyRunning ?? null)
-    );
 
-    const query = await FirestoreUtil.getByFilter(roadIntensityFactorsCollection, filter);
-    const queryData = FirestoreUtil.getDataFromQuerySnapshot(query);
-    
-    if(!(queryData.length > 1)){
-      throw new Error('The provided data was not enough to find the correct emission factor!')
-    } else if(!(queryData.length < 1)){
-      throw new Error('The provided data does not corespond to any emission factor!')
-    }
+  const filter = Filter.and(
+    Filter.where("fuel.code", "==", data.fuelCode ?? null),
+    Filter.or(
+      Filter.where("region", "==", region),
+      Filter.where("region", "==", "INTERNATIONAL")
+    ),
+    Filter.or(
+      Filter.where("vehicle.weight.lower", ">=", data.vehicle.weight.value),
+      Filter.where("vehicle.weight.upper", "<=", data.vehicle.weight.value),
+    ),
+    Filter.where("vehicle.weight.unit", "==", data.vehicle.weight.unit),
+    Filter.where("vehicle.code", "==", data.vehicle.code ?? null),
+    Filter.where("vehicle.engineType", "==", data.vehicle.engineType ?? null),
+    Filter.where("characteristics.loadCharacteristic", "==", data.characteristics.loadCharacteristic ?? null),
+    Filter.where("characteristics.loadFactor", "==", data.characteristics.loadFactor ?? null),
+    Filter.where("characteristics.combinedLoadFactorEmptyRunning", "==", data.characteristics.combinedLoadFactorEmptyRunning ?? null),
+    Filter.where("characteristics.emptyRunning", "==", data.characteristics.emptyRunning ?? null)
+  );
 
-    return queryData[0];
+  const queryData = await FirestoreUtil.getByFilter(roadIntensityFactorsCollection, filter);
+
+  const validatedQueryData = validateInput(
+    queryData,
+    z.array(roadIntensityFactorSchema),
+    "Received unexpected Road Intensity Factors format from the database."
+  )
+
+  if(!(validatedQueryData.length > 1)){
+    throw new Error("The provided data was not enough to find the correct emission factor!")
+  } else if(!(queryData.length < 1)){
+    throw new Error("The provided data does not corespond to any emission factor!")
+  }
+
+  return validatedQueryData[0];
 }
 
 export const RoadIntensityFactorService = {
