b13fa1f48ef4552e7f8ff47567780fd896234e41
Zali-Masodi <60880359+Zali-Masodi@users.noreply.github.com>
2023-11-25 13:18:26 +0100

    road intensity calculation started

--- functions/src/logic/emission_calculations/road_intensity_calculation.ts
+++ functions/src/logic/emission_calculations/road_intensity_calculation.ts
@@ -3,10 +3,94 @@ import {
   RoadTransportDetails,
   TransportActivityReport,
 } from "../../models/emission_calculations/emission_calculation_model";
+import { RoadIntensityFactorService } from "../emission_factors/intensity_factors/road_intensity_factor_service";
+import { validateInput } from "../../utils/functions";
+import { roadIntensityFactorSchema } from "../../models/emission_factors/road_intensity_factors";
+import { classifyUnitType } from "../units/unit_classification_service";
+import { EmissionFactorUtils } from "../../utils/emission_factor_utils";
+import { ACTIVITY_BASE_UNIT, baseEmissionReportFactory } from "../../utils/calculation_report";
+import { CustomError } from "../../utils/errors";
+import { HttpStatusCode } from "axios";
+import { UnitConversionService } from "../units/unit_conversion_service";
 
 export async function handleCalculationForRoadTransport(
   transportPart: FreightEmissionCalculationInput["transportParts"][number],
   transportDetails: RoadTransportDetails
 ): Promise<TransportActivityReport> {
-  throw new Error("Not implemented, TODO: Logic for road transport");
+
+  const factor = await RoadIntensityFactorService.getSpecificIntensityFactor(transportDetails, transportPart.region) 
+
+  const validatedRoadFactor = validateInput(
+    factor,
+    roadIntensityFactorSchema,
+    "Received unexpected Fuel Emission Factor format from the database."
+  );
+
+  // Get the factor with the same unit type as the provided unit type
+  const factorToUse = validatedRoadFactor.factor
+
+  if (!factorToUse) {
+    throw new CustomError({
+      status: HttpStatusCode.BadRequest,
+      message: `No emission factor was found!`,
+    });
+  }
+
+  
+  const producedEmissions = {
+    tankToWheel: factorToUse.ttw
+      ? 1 * factorToUse.ttw
+      : 0,
+    wellToTank: factorToUse.wtt
+      ? 1 * factorToUse.wtt
+      : 0,
+    wellToWheel: factorToUse.wtw
+      ? 1 * factorToUse.wtw
+      : 0,
+  };
+
+  if(validatedRoadFactor.refrigerated){
+      producedEmissions.tankToWheel = producedEmissions.tankToWheel * 1.5;
+      producedEmissions.wellToTank = producedEmissions.wellToTank * 1.5;
+      producedEmissions.wellToWheel = producedEmissions.wellToWheel * 1.5;
+  }
+
+
+  const km = UnitConversionService.convertUnits(
+    transportPart.distance.unit,
+    ACTIVITY_BASE_UNIT.DISTANCE,
+    transportPart.distance.value
+  ).value;
+
+  const tonnes = UnitConversionService.convertUnits(
+    transportPart.weight.unit,
+    ACTIVITY_BASE_UNIT.WEIGHT,
+    transportPart.weight.value
+  ).value;
+
+  const tkm = tonnes * km;
+  const emissionIntensity = producedEmissions.wellToWheel / tkm;
+
+  return {
+    input: transportPart,
+    emissionFactor: validatedRoadFactor,
+    mode: "ROAD",
+    emissions: baseEmissionReportFactory({
+      co2e:
+        producedEmissions.wellToWheel ??
+        // TODO: Is it the correct fallback to add the wellToTank and tankToWheel emissions in case the wellToWheel is not provided?
+        producedEmissions.wellToTank + producedEmissions.tankToWheel,
+      intensity: emissionIntensity,
+      activity: tkm,
+      distance: km,
+      wtt: {
+        co2e: producedEmissions.wellToTank,
+        intensity: producedEmissions.wellToTank / tkm,
+      },
+      ttw: {
+        co2e: producedEmissions.tankToWheel,
+        intensity: producedEmissions.tankToWheel / tkm,
+      },
+    }),
+  };
 }
