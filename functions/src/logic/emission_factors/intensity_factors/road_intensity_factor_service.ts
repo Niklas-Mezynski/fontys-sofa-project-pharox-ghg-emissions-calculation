a993c80beeb36488bf06ce650a8c3143093d330b
Joel Sebastian <j.sebastian@student.fontys.nl>
2023-11-30 14:39:39 +0100

    Add documentation to the filter in road intensity emission factors

--- functions/src/logic/emission_factors/intensity_factors/road_intensity_factor_service.ts
+++ functions/src/logic/emission_factors/intensity_factors/road_intensity_factor_service.ts
@@ -23,12 +23,11 @@ async function getSpecificIntensityFactor(
   region: string
 ): Promise<RoadIntensityFactor> {
 
-  
+  // Do a first filter on refrigereted vehicles and calculation region
   const filter = Filter.and(
     Filter.where("refrigerated", "==", data.refrigerated),
     Filter.where("region", "in", [region, "INTERNATIONAL"])
   );
-
 
   const queryData = await FirestoreUtil.getByFilter(
     roadIntensityFactorsCollection,
@@ -41,30 +40,35 @@ async function getSpecificIntensityFactor(
     "Received unexpected Road Intensity Factors format from the database."
   );
 
+  // Indicator to know if we have to look for a default road intensity emission factor
   let useDefault = true;
 
+  // Filter the data according to vehicle input if it exists
   const vehicle = data.vehicle
   if(vehicle){
+    // Do the filter if vehicle code and weight is provided
     if(vehicle.code && vehicle.weight && vehicle.weight.unit && vehicle.weight.value){
-      validatedQueryData = validatedQueryData.filter(d => 
-        (d.vehicle?.code === vehicle.code || vehicle.code && d.vehicle?.code.includes(vehicle.code)) 
+      validatedQueryData = validatedQueryData.filter(d =>
+        (d.vehicle?.code === vehicle.code || vehicle.code && d.vehicle?.code.includes(vehicle.code))
         && d.vehicle?.weight?.unit === vehicle.weight.unit
         && (
-          (d.vehicle.weight.upper && vehicle.weight.value < d.vehicle.weight.upper)
-          || ((d.vehicle.weight.lower && vehicle.weight.value >= d.vehicle.weight.lower && d.vehicle.weight.upper && vehicle.weight.value < d.vehicle.weight.upper)
-          || (d.vehicle.weight.lower && vehicle.weight.value >= d.vehicle.weight.lower))
+          (d.vehicle.weight.upper && vehicle.weight.value < d.vehicle.weight.upper) // Lower than upper limit
+          || ((d.vehicle.weight.lower && vehicle.weight.value >= d.vehicle.weight.lower && d.vehicle.weight.upper && vehicle.weight.value < d.vehicle.weight.upper) // Greater or equal to lower limit and lower than upper limit
+          || (d.vehicle.weight.lower && vehicle.weight.value >= d.vehicle.weight.lower)) // Greater or equal to lower limit
         )
       );
 
+      // Extra filter in case engine type is provided
       if(vehicle.engineType){
-        validatedQueryData = validatedQueryData.filter(d => 
+        validatedQueryData = validatedQueryData.filter(d =>
           d.vehicle?.engineType === vehicle.engineType
         );
         useDefault = false;
       }
     }
   }
-  
+
+  // Filter the data according to characteristics input if it exists
   const characteristics = data.characteristics
   if(characteristics){
     if(characteristics.emptyRunning){
@@ -88,15 +92,18 @@ async function getSpecificIntensityFactor(
     }
   }
 
+  // Filter the data according to fuel input if it exists
   const fuelCode = data.fuelCode
   if(fuelCode){
     validatedQueryData = validatedQueryData.filter( d => d.fuel?.code === fuelCode);
     useDefault = false;
   }
 
+  // In case we have more than one result, check if we can use the default road intensity emission factor
+  // Default factor -> only provides wehicle info (code and weight)
   if ((validatedQueryData.length > 1)) {
     if(useDefault && vehicle.code){
-      validatedQueryData = validatedQueryData.filter( d => 
+      validatedQueryData = validatedQueryData.filter( d =>
         (d.characteristics === null
         || (d.characteristics.combinedLoadFactorEmptyRunning === null
         && d.characteristics.emptyRunning === null
@@ -110,17 +117,16 @@ async function getSpecificIntensityFactor(
       throw new CustomError(
         {
           message: "The provided data was not enough to find the correct emission factor!",
-          status: HttpStatusCode.BadRequest
+          status: HttpStatusCode.BadRequest,
         });
     }
+  }
 
-  } 
-  
   if ((queryData.length === 0)) {
     throw new CustomError(
       {
         message: "The provided data does not corespond to any emission factor!",
-        status: HttpStatusCode.BadRequest
+        status: HttpStatusCode.BadRequest,
       }
     );
   }
