6a0856c9f565b8f22e64de040add04e71375f9e7
Joel Sebastian <j.sebastian@student.fontys.nl>
2023-12-06 15:31:48 +0100

    Update export vehicle and fuel scripts to be able to use multiple files

--- functions/data/scripts/export_different_vehicle_from_json.js
+++ functions/data/scripts/export_different_vehicle_from_json.js
@@ -1,23 +1,28 @@
 const fs = require("fs");
 
-const filePath = "../emission_intensity_factors/road/road_emission_intensity_factors.json";
-// const filePath = "../fuel_emission_factors/fuel_emission_factors.json";
+const filePaths = [
+  "../emission_intensity_factors/road/road_emission_intensity_factors.json",
+  "../fuel_emission_factors/fuel_emission_factors.json"
+];
 const vehicleFilePath = "../vehicle/vehicle.json";
 
 (async () => {
-  const data = fs.readFileSync(filePath, "utf8");
-  const parsedData = JSON.parse(data);
-
   const vehicleData = fs.readFileSync(vehicleFilePath, "utf8");
   const parsedVehicleData = (vehicleData) ? JSON.parse(vehicleData) : [];
 
-  for (const item of parsedData) {
-    if(!item.vehicle) continue;
+  for (const filePath of filePaths) {
+    const data = fs.readFileSync(filePath, "utf8");
+    const parsedData = JSON.parse(data);
 
-    const index = parsedVehicleData.findIndex((vehicle) => vehicle.code === item.vehicle.code);
 
-    if(index < 0) {
-      parsedVehicleData.push(item.vehicle);
+    for (const item of parsedData) {
+      if(!item.vehicle) continue;
+
+      const index = parsedVehicleData.findIndex((vehicle) => vehicle.code === item.vehicle.code);
+
+      if(index < 0) {
+        parsedVehicleData.push(item.vehicle);
+      }
     }
   }
 
